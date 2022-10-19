43f6d9fef4d1ec1269a63a232b0d5caf18f80362
Pharminous Nyabuto <pharminous@gmail.com>
2022-10-19 20:34:16 +0300

    add simple JS code to display the next image once the next button is clicked

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+function nextImage(){// simple Js code to change image src onclicking the next image button
+   let img = document.getElementById('myImage');
+   img.setAttribute('src', './images/devops2.png');
+}
